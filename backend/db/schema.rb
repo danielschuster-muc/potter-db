9649e64a896a2e95c4286bb9482399bac5074cd0
Daniel Schuster <daniel.a.schuster@t-online.de>
2022-07-18 14:37:47 +0200

    update gemfile and schema

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_13_204306) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_13_200257) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "characters", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -45,14 +44,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_13_204306) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["slug"], name: "index_characters_on_slug", unique: true
-  end
-
-  create_table "endpoints", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.string "link"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "spells", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
