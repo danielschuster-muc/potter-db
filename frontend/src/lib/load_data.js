882df08f82cc3197127efd488264106e1d93b72f
Daniel Schuster <daniel.a.schuster@t-online.de>
2022-08-03 23:38:22 +0200

    cleanup characters list code

--- frontend/src/lib/load_data.js
+++ frontend/src/lib/load_data.js
@@ -16,19 +16,19 @@ const defaultQuery = {
   page: 1,
   pageSize: 25,
   search: "",
-  sort: "",
+  orderBy: "",
   direction: "asc",
 };
-export const getAll = async (type, query = defaultQuery) => {
-  const { page, pageSize, search, sort, direction } = query;
 
+export const getAll = async (type, query = defaultQuery) => {
+  const { page, pageSize, search, orderBy, direction } = query;
   const pagination = `page[number]=${page}`;
   const perPage = `page[size]=${pageSize}`;
   const searchFilter = `filter[name_cont_any]=${search}`;
-  const sorting = `sort=${direction === "desc" ? "-" : ""}${sort}`;
+  const order = `sort=${direction === "desc" ? "-" : ""}${orderBy}`;
 
   return fetch(
-    `${apiUrl}/v1/${type}?${pagination}&${perPage}&${searchFilter}&${sorting}`
+    `${apiUrl}/v1/${type}?${pagination}&${perPage}&${searchFilter}&${order}`
   )
     .then((res) => res.json())
     .catch((error) => {
--- frontend/src/pages/characters/index.js
+++ frontend/src/pages/characters/index.js
@@ -1,58 +1,28 @@
-import {
-  FormControl,
-  Grid,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableFooter,
-  TableHead,
-  TableRow,
-  TableSortLabel,
-  Typography,
-} from "@mui/material";
+import { Typography } from "@mui/material";
+
 import Meta from "../../components/Meta";
+import DataListTable from "../../components/ui/DataListTable";
 import SearchField from "../../components/ui/SearchField";
 import { getCharacters } from "../../lib/load_characters";
-import SortFilter from "../../components/ui/SortFilter";
-import DirectionFilter from "../../components/ui/DirectionFilter";
-import CustomTablePagination from "../../components/ui/CustomTablePagination";
-import Link from "next/link";
-import { Box } from "@mui/system";
-import { visuallyHidden } from "@mui/utils";
-import { useState } from "react";
-import { useRouter } from "next/router";
-
-const headCells = [
-  "Name",
-  "Species",
-  "Gender",
-  "Nationality",
-  "House",
-  "Height",
-  "Weight",
-];
 
 const Characters = ({ characters }) => {
-  const router = useRouter();
   const { data, meta } = characters;
-  const [order, setOrder] = useState("asc");
-  const [orderBy, setOrderBy] = useState(headCells[0].toLowerCase());
+
+  const totalRecords = meta?.pagination.records;
 
   const metaDescription = `List of all Harry Potter characters - ${
-    meta ? `Total: ${meta.pagination.records}` : ""
+    meta ? `Total: ${totalRecords}` : ""
   }`;
 
-  const handleRequestSort = (property) => (_event) => {
-    const isAsc = orderBy === property && order === "asc";
-
-    const { pathname, query } = router;
-    query.direction = isAsc ? "desc" : "asc";
-    setOrder(isAsc ? "desc" : "asc");
-    query.sort = property;
-    setOrderBy(property);
-    router.push({ pathname, query });
-  };
+  const headCells = [
+    "Name",
+    "Species",
+    "Gender",
+    "Nationality",
+    "House",
+    "Height",
+    "Weight",
+  ];
 
   return (
     <>
@@ -60,69 +30,15 @@ const Characters = ({ characters }) => {
       <Typography variant="h3">
         Welcome to the Harry Potter Character List
       </Typography>
-      <SearchField totalResults={meta?.pagination.records} />
+
+      <SearchField totalResults={totalRecords} />
 
       {data && (
-        <TableContainer>
-          <Table>
-            <TableHead>
-              <TableRow>
-                {headCells.map((headCell) => {
-                  const id = headCell.toLowerCase();
-                  return (
-                    <TableCell
-                      key={id}
-                      sortDirection={orderBy === id ? order : false}
-                    >
-                      <TableSortLabel
-                        align="right"
-                        active={orderBy === id}
-                        direction={orderBy === id ? order : "asc"}
-                        onClick={handleRequestSort(id)}
-                      >
-                        {headCell}
-                        {orderBy === id ? (
-                          <Box component="span" sx={visuallyHidden}>
-                            {order === "desc"
-                              ? "sorted descending"
-                              : "sorted ascending"}
-                          </Box>
-                        ) : null}
-                      </TableSortLabel>
-                    </TableCell>
-                  );
-                })}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {data.map((character) => (
-                <TableRow
-                  key={character.id}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    <Link href={`/characters/${character.attributes.slug}`}>
-                      {character.attributes.name}
-                    </Link>
-                  </TableCell>
-                  <TableCell>{character.attributes.species}</TableCell>
-                  <TableCell>{character.attributes.gender}</TableCell>
-                  <TableCell>{character.attributes.nationality}</TableCell>
-                  <TableCell>{character.attributes.house}</TableCell>
-                  <TableCell>{character.attributes.height}</TableCell>
-                  <TableCell>{character.attributes.weight}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-            <TableFooter>
-              <TableRow>
-                <CustomTablePagination
-                  totalRecords={meta?.pagination.records}
-                />
-              </TableRow>
-            </TableFooter>
-          </Table>
-        </TableContainer>
+        <DataListTable
+          characters={data}
+          headCells={headCells}
+          totalRecords={totalRecords}
+        />
       )}
 
       {!data && <p>No characters available!</p>}
