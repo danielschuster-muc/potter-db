60028ea2c772ae8b7cfd8e3491c04ca2f38ca625
Daniel Schuster <daniel.a.schuster@t-online.de>
2022-09-05 22:35:57 +0200

    fix omit hash value

--- backend/app/controllers/pages_controller.rb
+++ backend/app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
     api_routes = Rails.application.routes.routes.map { |r| r.path.spec.to_s.gsub!("(.:format)", "") }.filter { |r| r&.start_with?("/v1") }
-    render json: { message: "welcome to the harry potter api!", api_routes: api_routes }
+    render json: { message: "welcome to the harry potter api!", api_routes: }
   end
 end
