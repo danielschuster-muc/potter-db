3833b504aa6f5455e823727f64d36f2c268ae5af
Daniel Schuster <daniel.a.schuster@t-online.de>
2022-09-27 22:27:59 +0200

    switch to button to load more data

--- frontend/src/components/pages/characters/CharacterListStatus.js
+++ frontend/src/components/pages/characters/CharacterListStatus.js
@@ -1,11 +1,40 @@
-import { CircularProgress, Typography } from "@mui/material";
+import { Error, Expand, Loop } from "@mui/icons-material";
+import { Button } from "@mui/material";
 
-const CharacterListStatus = ({ isLoading, error, isFetchingNextPage }) => {
+const CharacterListStatus = ({
+  isFetchingNextPage,
+  hasNextPage,
+  fetchNextPage,
+  error,
+}) => {
+  const message = "Show more characters";
+  if (isFetchingNextPage) {
+    message = "Loading more...";
+  } else if (error) {
+    message = `Error: ${error.message}`;
+  } else if (!hasNextPage) {
+    message = "No more characters available";
+  }
+
+  const icon = <Expand />;
+  if (isFetchingNextPage) {
+    icon = <Loop />;
+  } else if (error || !hasNextPage) {
+    icon = <Error />;
+  }
+
   return (
     <>
-      {isLoading && <CircularProgress />}
-      {error && <Typography>Error: {error.message}</Typography>}
-      {isFetchingNextPage && <Typography>Loading more...</Typography>}
+      <Button
+        size="small"
+        onClick={fetchNextPage}
+        variant="outlined"
+        startIcon={icon}
+        sx={{ my: 1 }}
+        disabled={isFetchingNextPage || !hasNextPage || error}
+      >
+        {message}
+      </Button>
     </>
   );
 };
