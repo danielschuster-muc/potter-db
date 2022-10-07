32e50ef8105b588faa38f5f5fc0cdf9831cbbd9c
Christopher Vestman <christophervestman@gmail.com>
2022-10-07 11:21:13 +0200

    fix: removed spell accordion and centered card

--- frontend/src/components/pages/spells/[slug]/SpellDetails.js
+++ frontend/src/components/pages/spells/[slug]/SpellDetails.js
@@ -1,26 +1,10 @@
 import { Grid } from '@mui/material'
 
-import AccordionList from '../../../ui/AccordionList';
 import DetailInfoCard from '../../../ui/DetailedInfoCard';
 
 
 const SpellDetails = ({ attributes, apiLink }) => {
   const { name, slug, effect, hand, incantation, category, light, creator, image, wiki } = attributes;
-
-  const accordions = [
-    {
-      name: 'Effect',
-      value: effect,
-    },
-    {
-      name: 'Hand',
-      value: hand,
-    },
-    {
-      name: 'Category',
-      value: category,
-    }
-  ];
 
   const informationTable = [
     {
@@ -28,22 +12,34 @@ const SpellDetails = ({ attributes, apiLink }) => {
       value: name,
     },
     {
-      name: 'Incantation',
+      name: 'incantation',
       value: incantation,
     },
     {
-      name: 'light',
+      name: 'Effect',
+      value: effect,
+    },
+    {
+      name: 'Category',
+      value: category,
+    },
+    {
+      name: 'Light',
       value: light,
     },
     {
-      name: 'creator',
+      name: 'Hand',
+      value: hand,
+    },
+    {
+      name: 'Creator',
       value: creator,
     },
-  ]
+  ];
 
   return (
     <>
-    <Grid container spacing={3}>
+    <Grid container spacing={3} justifyContent='center'>
       <Grid item xs={12} md={6}>
         <DetailInfoCard
           title={name}
@@ -56,10 +52,6 @@ const SpellDetails = ({ attributes, apiLink }) => {
           ]}
         />
       </Grid>
-      <Grid item xs={12} md={6}>
-        <AccordionList accordions={accordions} />
-      </Grid>
-
     </Grid>
     </>
   )
