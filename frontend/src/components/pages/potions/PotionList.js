26206d7744151427e41e60c63fe01d72b0ed8072
Eduard Constantin <ibinceanu.eduard@yahoo.com>
2022-10-07 19:33:41 +0300

    Indentation fix 2

--- frontend/src/components/pages/potions/PotionList.js
+++ frontend/src/components/pages/potions/PotionList.js
@@ -9,53 +9,53 @@ import SearchField from "../../ui/SearchField";
 import PotionListItem from "./PotionListItem";
 
 const PotionList = ({ fetchPotions }) => {
-	const [searchQuery, setSearchQuery] = useState();
+  const [searchQuery, setSearchQuery] = useState();
 
-	const {
-		data: rawPotions,
-		isFetchingNextPage,
-		isSuccess,
-		error,
-		hasNextPage,
-		fetchNextPage,
-	} = useInfiniteQuery(["potions", searchQuery], fetchPotions, {
-		retry: 10,
-		getNextPageParam: (lastPage, pages) => {
-			if (pages?.length < (lastPage?.meta?.pagination?.last || 0)) {
-				return pages.length + 1;
-			}
-			return undefined;
-		},
-	});
+  const {
+    data: rawPotions,
+    isFetchingNextPage,
+    isSuccess,
+    error,
+    hasNextPage,
+    fetchNextPage,
+  } = useInfiniteQuery(["potions", searchQuery], fetchPotions, {
+    retry: 10,
+    getNextPageParam: (lastPage, pages) => {
+      if (pages?.length < (lastPage?.meta?.pagination?.last || 0)) {
+        return pages.length + 1;
+      }
+      return undefined;
+    },
+  });
 
-	return (
-		<>
-			<Typography variant="h3">Potion Search</Typography>
-			<SearchField
-				placeholder="e.g. Dragon tonic"
-				handleChangeSearch={setSearchQuery}
-				totalResults={rawPotions?.pages ? rawPotions?.pages[0]?.meta?.pagination?.records : 0}
-			/>
-			{isSuccess && (
-				<InfiniteScroll hasMore={hasNextPage} loadMore={() => {}}>
-					<Grid container spacing={2}>
-						{rawPotions?.pages?.map((page) =>
-							page?.data?.map((potion) => {
-								return <PotionListItem key={potion.id} potion={potion} />;
-							})
-						)}
-					</Grid>
-				</InfiniteScroll>
-			)}
-			<ListStatusButton
-				isFetchingNextPage={isFetchingNextPage}
-				hasNextPage={hasNextPage}
-				fetchNextPage={fetchNextPage}
-				error={error}
-				title="potions"
-			/>
-		</>
-	);
+  return (
+    <>
+      <Typography variant="h3">Potion Search</Typography>
+      <SearchField
+        placeholder="e.g. Dragon tonic"
+        handleChangeSearch={setSearchQuery}
+        totalResults={rawPotions?.pages ? rawPotions?.pages[0]?.meta?.pagination?.records : 0}
+      />
+      {isSuccess && (
+        <InfiniteScroll hasMore={hasNextPage} loadMore={() => {}}>
+          <Grid container spacing={2}>
+            {rawPotions?.pages?.map((page) =>
+              page?.data?.map((potion) => {
+                return <PotionListItem key={potion.id} potion={potion} />;
+              })
+            )}
+          </Grid>
+        </InfiniteScroll>
+      )}
+      <ListStatusButton
+        isFetchingNextPage={isFetchingNextPage}
+        hasNextPage={hasNextPage}
+        fetchNextPage={fetchNextPage}
+        error={error}
+        title="potions"
+      />
+    </>
+  );
 };
 
 export default PotionList;
