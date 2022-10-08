982c86c05f3e78582eb1d515117a498753527260
chintan0513 <chintantripathi1310@gmail.com>
2022-10-08 22:43:59 +0530

    added logo.svg

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,8 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     domains: ["static.wikia.nocookie.net", "www.wizardingworld.com"]
   },
   productionBrowserSourceMaps: true,
--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
       <Toolbar disableGutters>
         <Link href="/" style={{ textDecoration: 'none' }}>
           <Box display="flex">
-          <Image src="/images/logo.svg" width={30} height={10} alt="logo" />
+          <Image src='/images/logo.svg' width={25} height={25} alt="logo" />
             <Typography
               variant="h5"
               noWrap
