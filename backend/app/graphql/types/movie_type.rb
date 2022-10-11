483e953d32c57128e208a154efb6680bb610e503
Daniel Schuster <daniel.a.schuster@t-online.de>
2022-10-11 16:09:17 +0200

    fix duplicate fields in movie type

--- backend/app/graphql/types/movie_type.rb
+++ backend/app/graphql/types/movie_type.rb
@@ -22,25 +22,6 @@ module Types
     field :trailer, String
     field :poster, String
     field :wiki, String
-    field :slug, String
-    field :title, String
-    field :summary, String
-    field :directors, String
-    field :screenwriters, String
-    field :producers, String
-    field :cinematographers, String
-    field :editors, String
-    field :distributors, String
-    field :music_composers, String
-    field :release_date, String
-    field :running_time, String
-    field :budget, String
-    field :box_office, String
-    field :rating, String
-    field :order, Integer
-    field :trailer, String
-    field :poster, String
-    field :wiki, String
     # field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     # field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
