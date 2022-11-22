c39a0ba6b11ab32742649796b1e06538f35cca83
gdscghrcem <themarketingkirtan@gmail.com>
2022-11-22 20:11:08 +0530

    made 404 page 1

--- frontend/src/pages/404.js
+++ frontend/src/pages/404.js
@@ -1,7 +1,21 @@
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 
 const NotFound = () => {
-  return <Typography variant="h3">404 - Page Not Found</Typography>;
+  return (
+    <div>
+      <Typography variant="h3">Have You Seen this page?</Typography>
+      <img
+        src="https://user-images.githubusercontent.com/90754725/194039761-45ed2b6c-aa69-4070-b1df-6ca2ef90cb48.png"
+        alt=""
+        srcset=""
+      />
+      <Typography variant="body1">
+        Approach with extreme caution! To not attempt to use magic against this
+        site!
+      </Typography>
+      <Button href="/">Back to Home</Button>
+    </div>
+  );
 };
 
 export default NotFound;
