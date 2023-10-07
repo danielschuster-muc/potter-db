ea4f3d299abe39d485a4dce67981ca681f44ebb0
alexvcs <alexcurtisslep@gmail.com>
2023-10-06 20:07:56 -0400

    working on adding progress bar

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,10 @@
+"use client";
 import React from "react";
 import type { Metadata } from "next";
 
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 import "./../styles/globals.css";
 
 export const metadata: Metadata = {
@@ -17,6 +19,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <ProgressBar height="4px" color="#D926D9" options={{ showSpinner: false }} shallowRouting />
       <body>
         <div className="flex flex-col min-h-screen mx-6">
           <Navbar />
