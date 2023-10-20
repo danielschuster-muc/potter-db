35fa435f208cdda3dbb4e2bc93b5bac0fb493223
Andre Westerlund <20700150+westerandr@users.noreply.github.com>
2023-10-20 21:58:45 +1300

    Update frontend/src/components/ui/LoadMoreButton.tsx
    Co-authored-by: Daniel Schuster <90754725+danielschuster-muc@users.noreply.github.com>

--- frontend/src/components/ui/LoadMoreButton.tsx
+++ frontend/src/components/ui/LoadMoreButton.tsx
@@ -18,7 +18,7 @@ export default function LoadMoreButton({
       className="bg-secondary text-white opacity-90 hover:opacity-100 rounded-lg py-2 px-6 mt-4 disabled:opacity-50 disabled:cursor-not-allowed"
       onClick={() => setSize(size + 1)}
       disabled={isLoading || results.length >= totalResults}>
-      {results.length >= totalResults ? `No more characters` : "Load more"}
+      {results.length >= totalResults ? "No more characters" : "Load more"}
     </button>
   );
 }
