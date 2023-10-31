2693c0c8805c1ec1e8ab844aab2f6ce2b430cbfa
Camila Franco <camilafrancocnf@gmail.com>
2023-10-31 12:57:51 -0300

    Updated files

--- docs/next.config.js
+++ docs/next.config.js
@@ -11,7 +11,7 @@ const withNextra = require("nextra")({
 module.exports = withNextra({
   reactStrictMode: true,
   i18n: {
-    locales: ["en", "fr", "es", "sk", "de"],
+    locales: ["en", "fr", "es", "sk", "de", "pt"],
     defaultLocale: "en",
   },
 });
