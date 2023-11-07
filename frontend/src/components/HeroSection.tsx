d6e82d153ce95a364e96a36fbfc07d595869e1b3
Daniel Schuster <daniel.a.schuster@t-online.de>
2023-11-07 21:52:58 +0100

    improve landing page image

--- frontend/src/components/HeroSection.tsx
+++ frontend/src/components/HeroSection.tsx
@@ -6,20 +6,19 @@ import hogwartsExpress from "../../public/images/hogwarts_express.jpg";
 export default function HeroSection() {
   return (
     <section className="relative w-full h-[80vh] rounded-lg">
-      <div className="relative w-full h-[80vh]">
-        <Image
-          src={hogwartsExpress}
-          alt="Hogwarts Express"
-          className="rounded-lg object-left-top w-full h-full object-cover"
-          quality={75}
-          priority
-        />
-        <div className="border border-secondary rounded-lg absolute inset-0 flex flex-col items-center justify-center z-10 bg-gray-700 bg-opacity-30 h-full mx-auto px-3 text-center">
-          <h1 className="text-4xl md:text-6xl font-bold text-white">Potter DB</h1>
-          <h2 className="text-lg md:text-2xl text-white mt-2">
-            A Harry Potter database for all your wizarding needs.
-          </h2>
-        </div>
+      <Image
+        src={hogwartsExpress}
+        alt="Hogwarts Express"
+        fill
+        className="rounded-lg object-left-top object-cover"
+        quality={30}
+        priority
+      />
+      <div className="text-white border border-secondary rounded-lg absolute inset-0 flex flex-col items-center justify-center z-10 bg-gray-700 bg-opacity-30 h-full mx-auto px-3 text-center">
+        <h1 className="text-4xl md:text-6xl font-bold">Potter DB</h1>
+        <h2 className="text-lg md:text-2xl mt-2">
+          A Harry Potter database for all your wizarding needs.
+        </h2>
       </div>
     </section>
   );
